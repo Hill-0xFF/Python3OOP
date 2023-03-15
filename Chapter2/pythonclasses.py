9a9446b4aaa470efe179137557b8b20a3e1aef95
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-03-15 15:26:47 -0400

    Chapter 2 - adding new methods

--- Chapter2/pythonclasses.py
+++ Chapter2/pythonclasses.py
@@ -1,8 +1,32 @@
+import math
+
 class Point:
+  def move(self, x: int, y: int):
+    self.x = x
+    self.y = y
+
   def reset(self):
     self.x = 0
     self.y = 0
 
+  def calculateDistance(self, otherPoint):
+    return math.sqrt(
+      (self.x - otherPoint.x) ** 2 + 
+      (self.y - otherPoint.y) ** 2
+    )
+
 pnt1 = Point()
+pnt2 = Point()
+
 pnt1.reset()
+pnt2.move(5, 0)
+print(pnt2.calculateDistance(pnt1))
+
+assert(pnt2.calculateDistance(pnt1) == 
+  pnt1.calculateDistance(pnt2))
+
+pnt1.move(3,4)
+print (pnt1.calculateDistance(pnt2))
+print(pnt2.calculateDistance(pnt1))
+
 print("Px: {} Py: {}".format(pnt1.x, pnt1.y))
