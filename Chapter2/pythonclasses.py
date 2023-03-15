6f75cca683ded4bb6174333c39eceb860b84759d
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-03-14 20:45:48 -0400

    Chapter 2 - adding attributes - page 29

---
+++ Chapter2/pythonclasses.py
@@ -0,0 +1,14 @@
+class Point:
+  pass
+
+pnt1 = Point()
+pnt2 = Point()
+
+pnt1.x: int = 5
+pnt1.y: int = 4
+
+pnt2.x: int = 3
+pnt2.y: int = 6
+
+print("P1.X {} P1.Y {}".format(pnt1.x, pnt1.y))
+print("P2.X {} P2.Y {}".format(pnt2.x, pnt2.y))
