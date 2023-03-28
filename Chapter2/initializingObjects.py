a1b6c5e044aad006ea44cffab3bdc0e387c622ad
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-03-28 13:06:00 -0400

    Chapter 2 - documenting the module with docstrings

--- Chapter2/initializingObjects.py
+++ Chapter2/initializingObjects.py
@@ -1,13 +1,38 @@
+import math
+
 class Point:
+  'Representing points in a two-dimensional geometric coordinates'
   def __init__(self, x: int = 0, y: int = 0) -> int:
+    '''
+    Initialize the position of a new point. The X and Y coordinates can be specified.
+    If not specified, it'll set default value, which is 0.
+    '''
     self.move(x, y)
 
   def move(self, x: int, y: int) -> None:
+    '''
+    Move the point to a new location in a 2D space
+    '''
     self.x = x
     self.y = y
 
   def reset(self) -> None:
+    '''
+    Sets a new point in a 2D space back to origin: 0, 0
+    '''
     self.move(0, 0)
+
+  def calculateDistance(self, otherPoint) -> float:
+    '''
+    Calculate the distance between two points in a 2D space.
+    Uses the Pythagorean Theorem calculate the said distance.
+    Return value as float
+    '''
+    return math.sqrt(
+      (self.x - otherPoint.x) ** 2 +
+      (self.y - otherPoint.y) ** 2
+    )
+
 
 pnt1 = Point(1, 3)
 print(pnt1.x, pnt1.y)
