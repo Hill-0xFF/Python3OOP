0d8515cb11ddb1ac4b237a3b2958b14d2790ec9a
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-04-05 11:11:50 -0400

    Chapter 3 - defining a class for all contact list

--- Chapter3/contactsManager.py
+++ Chapter3/contactsManager.py
@@ -1,5 +1,18 @@
+class ContactList (list):
+  def search(self, name):
+    '''
+    Return all contacts that contain the search value
+    in their name.
+    '''
+    matching_contacts = []
+    for contact in self:
+      if name in contact.name:
+        matching_contacts.append(contact)
+    return matching_contacts
+
 class Contact:
-  allContacts: list = []
+  # allContacts: list = []
+  allContacts = ContactList()
   def __init__(self, name: str, email: str):
     self.name = name
     self.email = email
